6cf6f3299ff06b33ed907b0e3a371621c25ba9db
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2021-01-22 15:36:53 +0100

    Update Assignment_4_Cursors.sql

--- Assignment_4_Cursors.sql
+++ Assignment_4_Cursors.sql
@@ -1,1 +1,26 @@
+-- Cursors
+
+-- 1
+
+create or replace function vPresintoPres() returns void as $$
+declare
+c_vpres cursor for
+select distinct av.vice_pres_name from admin_vpres av;
+begin
+for vpres in c_vpres loop
+if (vpres.vice_pres_name in (select "name" from president)) then
+insert into vpresandpres values (vpres.vice_pres_name);
+end if;
+end loop;
+end;
+$$ language plpgsql;
+
+CREATE TEMP TABLE vpresandpres(
+    "name" varchar
+);
+
+select vPresintoPres();
+
+select * from vpresandpres;
+
 
