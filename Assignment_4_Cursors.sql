5743072d4c3b52f339b5d1c28d3f72ca6e825f69
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2021-01-22 16:13:08 +0100

    Update Assignment_4_Cursors.sql

--- Assignment_4_Cursors.sql
+++ Assignment_4_Cursors.sql
@@ -45,3 +45,26 @@ create temp table totalChildren (
 select nrChildren();
 
 select * from totalChildren;
+
+						
+						
+-- 4
+create or replace function nrElections() returns void as $$
+declare
+c_pres cursor for
+select id, "name" from president p;
+begin
+for pres in c_pres loop
+insert into nr_elections values (pres."name", (select count(e2.election_year) from election e2 where e2.candidate = pres."name"));
+end loop;
+end;
+$$ language plpgsql;
+
+create temp table nr_elections (
+	pres_name varchar,
+	nr_elections int
+);
+
+select nrelections();
+
+select * from nr_elections;
